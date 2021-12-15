bc0eb822055329fdc58759c1da3365140a2a9aed
Flash <34082891+nmbeilke@users.noreply.github.com>
2021-12-14 23:05:52 -0800

    Added to DB store.js

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -4,12 +4,12 @@ const router = require('express').Router()
 
 //Sends the notes.html file when the /notes url is used
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(_dirname, '../public/notes.html'))
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
 
 //Sends the homepage for any other url
 router.get('*', (req, res) => {
-    res.sendFile(path.join(_dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 //Exports
